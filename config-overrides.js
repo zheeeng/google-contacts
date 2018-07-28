74bc580930737cab493824eae8b8d3b323d177b3
jiangzheng <jiangzheng@yidian-inc.com>
2018-07-28 17:16:24 +0800

    Apply webpack configs overriding

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+module.exports = function override (config, env) {
+  const extendAlias = {
+    '~src': path.resolve(__dirname, 'src')
+  }
+
+  if (config.resolve) {
+    if (config.resolve.alias) {
+      Object.assign(config.resolve.alias, extendAlias)
+    } else {
+      config.resolve.alias = extendAlias
+    }
+  } else {
+    config.resolve = {
+      alias: extendAlias
+    }
+  }
+
+  return config
+}
