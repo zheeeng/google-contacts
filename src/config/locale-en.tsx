3ed14a0be88a285039dae31b3e2413e893e7b6df
Zheeeng <hi@zheeeng.com>
2018-07-29 17:21:31 +0800

    Add configs

---
+++ src/config/locale-en.tsx
@@ -0,0 +1,15 @@
+import { DomainPresets } from '~src/Context/locale.interface'
+
+const en: DomainPresets = {
+  message: {
+    AUTH_UNINITIALIZED: 'API Request is not ready',
+    AUTH_IS_SIGNING_IN: 'Account is logging in, please wait a moment',
+    AUTH_IS_SIGNING_OUT: 'Account is logging out, please wait a moment',
+  },
+  UI: {
+    loginButton: 'Login with Goole Account',
+    contacts: 'contacts',
+  },
+}
+
+export default en
---
+++ src/config/locale-zh.tsx
@@ -0,0 +1,15 @@
+import { DomainPresets } from '~src/Context/locale.interface'
+
+const en: DomainPresets = {
+  message: {
+    AUTH_UNINITIALIZED: 'API 请求还未准备好',
+    AUTH_IS_SIGNING_IN: '正在登录，请稍后再试',
+    AUTH_IS_SIGNING_OUT: '正在登出，请稍后再试',
+  },
+  UI: {
+    loginButton: '使用谷歌帐号登录',
+    contacts: '联系人',
+  },
+}
+
+export default en
