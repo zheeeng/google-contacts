633281823e39449ea5a8816155ad02d8431f6931
Zheeeng <hi@zheeeng.com>
2018-07-30 01:13:23 +0800

    Add mock pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Router, Redirect } from '@reach/router'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import indigo from '@material-ui/core/colors/indigo'
+import Typography from '@material-ui/core/Typography'
 
 import {
   servletHub,
@@ -21,12 +22,20 @@ const theme = createMuiTheme({
   },
 })
 
-const Fallback = () => <div>Fallback</div>
+const Fallback = () => <Typography variant="headline">页面不存在</Typography>
+const Contacts = () => <Typography variant="headline">最近联系人</Typography>
+const Frequently = () => <Typography variant="headline">常用联系人</Typography>
+const Duplicates = () => <Typography variant="headline">重复联系人</Typography>
+const Label = () => <Typography variant="headline">标签</Typography>
+const Settings = () => <Typography variant="headline">设置</Typography>
+const Feedback = () => <Typography variant="headline">反馈</Typography>
+const Help = () => <Typography variant="headline">帮助</Typography>
+const Old = () => <Typography variant="headline">旧版 Google Contacts</Typography>
 
 class App extends React.Component<Props> {
 
-  private renderAuthOrMain () {
-    return this.props.authService && !this.props.authService.isSignedIn
+  private renderAuthOrMain = () =>
+    this.props.authService && !this.props.authService.isSignedIn
       ? (
         <Router>
           <Redirect noThrow from="*" to="/sign-in" />
@@ -34,11 +43,20 @@ class App extends React.Component<Props> {
         </Router>
       ) : (
         <Router>
-          <Main path="/" />
+          <Main path="/">
+            <Contacts path="/contacts" />
+            <Frequently path="/frequent" />
+            <Duplicates path="/duplicates" />
+            <Label path="/label" />
+            <Settings path="/settings" />
+            <Feedback path="/feedback" />
+            <Help path="/help" />
+            <Old path="/old" />
+            <Fallback default />
+          </Main>
           <Fallback default />
         </Router>
       )
-  }
 
   render () {
     return (
