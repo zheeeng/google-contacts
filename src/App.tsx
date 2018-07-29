8ecdde01226373ea380f6e63d143018327156892
Zheeeng <hi@zheeeng.com>
2018-07-29 22:29:09 +0800

    Add base framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,53 @@
 import React from 'react'
-import './App.css'
+import { Router, Redirect } from '@reach/router'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import indigo from '@material-ui/core/colors/indigo'
 
-class App extends React.Component {
+import {
+  servletHub,
+  AuthServletProps,
+} from '~src/Context/GAPI'
+import { provideLocales, localize } from '~src/Context/Locale'
+
+import Main from '~src/components/Main'
+import SignIn from '~src/components/SignIn'
+
+interface Props extends AuthServletProps {}
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+  },
+})
+
+const Fallback = () => <div>Fallback</div>
+
+class App extends React.Component<Props> {
+
+  private renderAuthOrMain () {
+    return this.props.authService && !this.props.authService.isSignedIn
+      ? (
+        <Router>
+          <Redirect noThrow from="*" to="/sign-in" />
+          <SignIn path="/sign-in" />
+        </Router>
+      ) : (
+        <Router>
+          <Main path="/" />
+          <Fallback default />
+        </Router>
+      )
+  }
+
   render () {
     return (
-      <div className="App">
-        Hello
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        {this.renderAuthOrMain()}
+      </MuiThemeProvider>
     )
   }
 }
 
-export default App
+export default provideLocales(localize(servletHub(App)))
