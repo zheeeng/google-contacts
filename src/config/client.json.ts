a647ebba08c1aa2857f55a9b15e566e910b7ea9e
jiangzheng <jiangzheng@yidian-inc.com>
2018-07-29 17:21:31 +0800

    Add configs

---
+++ src/config/client.json.ts
@@ -0,0 +1,10 @@
+type InitOption =  FirstParam<typeof gapi.client.init>
+
+const config: InitOption =  {
+  apiKey: process.env.REACT_APP_GOOGLE_APP_KEY,
+  discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'],
+  clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
+  scope: 'https://www.googleapis.com/auth/contacts.readonly',
+}
+
+export default config
