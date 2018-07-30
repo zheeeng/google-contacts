4137c33108c365b95cd89635f8f0320f39ea7d67
jiangzheng <jiangzheng@yidian-inc.com>
2018-07-30 12:20:54 +0800

    Update Sign Component

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,22 +1,52 @@
 import React from 'react'
-import { Link } from '@reach/router'
+import { withStyles, WithStyles, createStyles, Theme } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 import OpenInNew from '@material-ui/icons/OpenInNew'
 
-export default class SignIn extends React.PureComponent {
+const styles = (theme: Theme) => createStyles({
+  root: {
+    width: '100vw',
+    height: '100vh',
+    flexGrow: 1,
+    overflow: 'hidden',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: theme.palette.background.paper,
+  },
+  heading: {
+    marginBottom: theme.spacing.unit * 3,
+  },
+  subHeading: {
+    marginBottom: theme.spacing.unit * 5,
+  },
+  signInIcon: {
+    marginRight: theme.spacing.unit,
+  },
+})
+
+interface Props extends WithStyles<typeof styles> {
+}
+class SignIn extends React.PureComponent<Props> {
   render () {
+    const { classes } = this.props
     return (
-      <div>
-        <h1>Welcome to the Contacts APP</h1>
+      <div className={classes.root}>
+        <Typography variant="headline" className={classes.heading}>
+          Welcome to the Google Contact APP
+        </Typography>
+        <Typography variant="subheading" className={classes.subHeading}>
+          Created witch stacks: React, TypeScript, Material-ui, Reach-Router
+        </Typography>
         <Button variant="contained" color="primary">
-          <OpenInNew style={{ marginRight: 8 }} />
+          <OpenInNew className={classes.signInIcon} />
           Sign in with Google Account
         </Button>
-        <nav>
-          <Link to="/">Home</Link> |{' '}
-          <Link to="dashboard">Dashboard</Link>
-        </nav>
       </div>
     )
   }
 }
+
+export default withStyles(styles)(SignIn)
