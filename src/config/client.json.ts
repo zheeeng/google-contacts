52fa03d302753315e38c9dec8c839e1973053178
Zheeeng <hi@zheeeng.com>
2018-07-30 15:52:10 +0800

    Update gapi scope

--- src/config/client.json.ts
+++ src/config/client.json.ts
@@ -4,7 +4,7 @@ const config: InitOption =  {
   apiKey: process.env.REACT_APP_GOOGLE_APP_KEY,
   discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/people/v1/rest'],
   clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-  scope: 'https://www.googleapis.com/auth/contacts.readonly',
+  scope: 'https://www.googleapis.com/auth/contacts',
 }
 
 export default config
